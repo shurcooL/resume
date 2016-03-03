98a9faf2bffe84eb3e6eaee9dd486dd33879bd20
Dmitri Shuralyov <shurcooL@gmail.com>
2016-03-02 22:34:39 -0800

    Use time.Month.

--- date.go
+++ date.go
@@ -10,11 +10,11 @@ import (
 
 // Date represents an imprecise date.
 type Date struct {
-	Year  int // Year. E.g., 2009.
-	Month int // Month is 1 - 12. 0 means unspecified. // TODO: Use time.Month.
+	Year  int        // Year. E.g., 2009.
+	Month time.Month // Month is 1 - 12. 0 means unspecified.
 }
 
-func (d Date) Date() (year int, month int) { return d.Year, d.Month }
+func (d Date) Date() (year int, month time.Month) { return d.Year, d.Month }
 
 func (d Date) Render() ([]*html.Node, error) {
 	switch d.Month {
@@ -28,9 +28,9 @@ func (d Date) Render() ([]*html.Node, error) {
 // Present represents the present date.
 type Present struct{}
 
-func (Present) Date() (year int, month int) {
-	y, m, _ := time.Now().UTC().Date()
-	return y, int(m)
+func (Present) Date() (year int, month time.Month) {
+	year, month, _ = time.Now().UTC().Date()
+	return year, month
 }
 
 func (p Present) Render() ([]*html.Node, error) {
@@ -38,7 +38,7 @@ func (p Present) Render() ([]*html.Node, error) {
 }
 
 type Dater interface {
-	Date() (year int, month int)
+	Date() (year int, month time.Month)
 	Component
 }
 
@@ -61,7 +61,7 @@ func (dr DateRange) Render() ([]*html.Node, error) {
 func yearsMonths(from, to Dater) string {
 	y0, m0 := from.Date()
 	y1, m1 := to.Date()
-	months := (y1-y0)*12 + m1 - m0
+	months := (y1-y0)*12 + int(m1) - int(m0)
 	years, months := months/12, months%12
 	switch {
 	case years == 0:
