a74416b74c37bb0573e7ad9c89a513aaf1a1555c
Dmitri Shuralyov <dmitri@shuralyov.com>
2018-01-15 14:48:51 -0500

    Add Open Source Contributor experience for 2017.

--- resume.go
+++ resume.go
@@ -44,6 +44,18 @@ func DmitriShuralyov(shurcool users.User, reactions map[string][]reactions.React
 
 			Items: []resumecomponent.Item{
 				{
+					Title: "Full-time Open Source Contributor",
+					Dates: resumecomponent.DateRange{
+						From: resumecomponent.Date{Year: 2016, Month: time.November}, To: resumecomponent.Date{Year: 2017, Month: time.December},
+					},
+					Lines: []htmlg.Component{
+						component.Join("Designed and created the first ", component.Link{Text: "GraphQL client library", URL: "https://github.com/shurcooL/githubql", NewTab: true}, " for Go."),
+						component.Text("Contributed code, performed code review and triaged issues for many popular packages to help sustain and expand the Go ecosystem."),
+						component.Text("Maintained the GopherJS compiler and implemented support for new releases of Go."),
+						component.Text("Implemented components that enable decentralized self-hosting of Go packages, including an issue tracker, git server, code viewer and code review tool."),
+					},
+				},
+				{
 					Title:    "Senior Software Engineer, Full Stack",
 					Subtitle: "Sourcegraph",
 					Dates: resumecomponent.DateRange{
